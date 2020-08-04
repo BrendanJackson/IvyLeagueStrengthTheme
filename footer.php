143e800e67d52f63330d45e95b12ecf9f4bdc9c2
brendan <bajackson859@gmail.com>
2020-08-04 19:02:33 -0400

    initial commit

---
+++ footer.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WP_Bootstrap_Starter
+ */
+
+?>
+
+<?php if(!is_page_template( 'blank-page.php' ) && !is_page_template( 'blank-page-with-container.php' )): ?>
+			</div><!-- .row -->
+		</div><!-- .container -->
+	</div><!-- #content -->
+    <?php get_template_part( 'footer-widget' ); ?>
+	<footer id="colophon" class="site-footer <?php echo wp_bootstrap_starter_bg_class(); ?>" role="contentinfo">
+		<div class="container pt-3 pb-3">
+            <div class="site-info">
+                &copy; <?php echo date('Y'); ?> <?php echo '<a href="'.home_url().'">'.get_bloginfo('name').'</a>'; ?>
+                <span class="sep"> | </span>
+                <a class="credits" href="/contact-us" target="_blank" title="" alt="">Contact Us</a>
+
+            </div><!-- close .site-info -->
+		</div>
+	</footer><!-- #colophon -->
+<?php endif; ?>
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+<script type="text/javascript" src="//downloads.mailchimp.com/js/signup-forms/popup/unique-methods/embed.js" data-dojo-config="usePlainJson: true, isDebug: false"></script><script type="text/javascript">window.dojoRequire(["mojo/signup-forms/Loader"], function(L) { L.start({"baseUrl":"mc.us3.list-manage.com","uuid":"f8db2c96fc32e7dae879a1409","lid":"30d3cefca9","uniqueMethods":true}) })</script>
+
+</body>
+</html>
